b7c4b5487f6633792616ad8270883b8724e3e5b9
Lucas Cesca <lucascesca14@gmail.com>
2024-06-09 09:25:29 -0300

    Selection Sort 2 added

---
+++ capitulo2/selection_sort/src/SelectionSort2.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class SelectionSort2 {
+    public static void main(String[] args) {
+        int[] arr = {5, 3, 6, 2, 10};
+        selectionSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i+1; j < arr.length; j++) {
+                if (arr[j] < arr[i]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
+}
