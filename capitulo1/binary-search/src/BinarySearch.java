e05f483ad17a0180f25aedb53bd0b0489c78abe2
Lucas Cesca <lucascesca14@gmail.com>
2024-06-22 10:19:58 -0300

    Recursive Binary Search

--- capitulo1/binary-search/src/BinarySearch.java
+++ capitulo1/binary-search/src/BinarySearch.java
@@ -1,8 +1,10 @@
 public class BinarySearch {
     public static void main(String[] args) {
-        int[] list = {1, 3, 5, 7, 9};
-        System.out.println(binarySearch(list, 9));
-        System.out.println(binarySearch(list, -1));
+        int[] list = {1, 3, 5, 7, 9, 23};
+        System.out.println(binarySearch(list, 23));
+        System.out.println(binarySearch(list, 59));
+        System.out.println(recursiveBinarySearch(list, 0, list.length, 23));
+        System.out.println(recursiveBinarySearch(list, 0, list.length, 59));
     }
 
     // Returns the position of the number passed as argument
@@ -32,5 +34,21 @@ public class BinarySearch {
     public static boolean isListEmpty(int[] list) {
         int listSize = list.length;
         return listSize == 0;
+    }
+
+    private static Integer recursiveBinarySearch(int[] list, int low, int high, int item) {
+        int mid = low + high / 2;
+
+        if (high == 0 || mid == list.length) {
+            return null;
+        }
+
+        if (list[mid] == item) {
+            return mid;
+        } else if (list[mid] > item) {
+            return recursiveBinarySearch(list, low, high-1, item);
+        } else {
+            return recursiveBinarySearch(list, low + 1, high, item);
+        }
     }
 }
